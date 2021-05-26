8d6d4a39200fe78ba862ebe0178881af2d059571
Kirill <kirill-prohor@yandex.ru>
2021-05-27 00:12:10 +0300

    00:12

--- src/scripts/accordeon.js
+++ src/scripts/accordeon.js
@@ -1,5 +1,4 @@
 let accordeonLabel = document.querySelectorAll('.accordeon__label')
-console.log(accordeonLabel);
 accordeonLabel.forEach(elem => {
     elem.addEventListener('click', e =>{
         e.currentTarget.children[0].children[0].classList.toggle('accordeon__label-text--active')
--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,19 +1,37 @@
 import Swiper, {Navigation} from 'swiper';
 Swiper.use([Navigation])
-const swiper = new Swiper('.catalog__slider', {
-    slidesPerView: 1.5,
-    spaceBetween: 30,
-    freeMode: true,
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 20
-    //     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const swiperCatalog = new Swiper('.catalog__slider', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  freeMode: true,
+  // breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20
+  //     },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperDoors = new Swiper('.inside-door__swiper', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  freeMode: true,
+  // breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20
+  //     },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
