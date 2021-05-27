d6afb0e9e14cd3399e72fbf9e3debd9679f4d2ea
Kirill <kirill-prohor@yandex.ru>
2021-05-27 15:25:13 +0300

    Add slider partners

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -18,7 +18,43 @@ const swiperCatalog = new Swiper('.catalog__slider', {
   },
 });
 
-const swiperDoors = new Swiper('.inside-door__swiper', {
+const swiperSertificat = new Swiper('.inside-door__swiper', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  freeMode: true,
+  // breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20
+  //     },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperDoors = new Swiper('.sertificat-swiper', {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  freeMode: true,
+  // breakpoints: {
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 20
+  //     },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperPartners = new Swiper('.partners__swiper', {
   slidesPerView: 1.5,
   spaceBetween: 30,
   freeMode: true,
